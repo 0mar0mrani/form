2ba68bee9ba4a5eef527600a997123704b9db2a7
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2022-12-22 22:04:06 +0100

    Set up state for firstName and LastName

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,5 +2,19 @@ import { useState, useEffect } from 'react';
 
 export default function Form() {
 	const [ isFormValid, setFormValidation ] = useState(false);
-	
+	const [ currentData, setCurrentData ] = useState({
+		firstName: {
+			value: '',
+			isValid: false,
+			type: 'name',
+			message: '',
+		},
+
+		lastName: {
+			value: '',
+			isValid: false,
+			type: 'name',
+			message: '',
+		},
+	})
 }
