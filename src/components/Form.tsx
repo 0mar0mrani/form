78eef90a638be17e4fa6b0582c776906ebe666c3
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2022-12-22 22:27:35 +0100

    Imported components and added handlers for text input

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,7 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
+
+import TextInput from './form/TextInput.tsx'
+import PrimaryButton from './form/PrimaryButton.tsx'
 
 export default function Form() {
 	const [ isFormValid, setFormValidation ] = useState(false);
@@ -17,4 +20,36 @@ export default function Form() {
 			message: '',
 		},
 	})
+
+	function handleFirstNameInput(event) {
+		const key = 'firstName';
+		const textInputValue = event.currentTarget.value;
+
+	}
+
+	function handleLastNameInput(event) {
+		const key = 'lastName';
+		const textInputValue = event.currentTarget.value;
+	}
+
+	return (
+		<form action="/">
+			<TextInput 
+				placeholderText="Fornavn" 
+				onInput={handleFirstNameInput} 
+				// message={}
+			/>
+
+			<TextInput 
+				placeholderText="Etternavn" 
+				onInput={handleLastNameInput} 
+				// message={}
+			/>
+
+			<PrimaryButton 
+				// submitMethod={} 
+				isDisabled={!isFormValid}
+			/>
+		</form>
+	)
 }
