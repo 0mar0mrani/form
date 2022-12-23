5083660f9c1a08f5c14d82e928b6299c6fac432e
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2022-12-23 14:47:35 +0100

    Created select input module

---
+++ src/components/form/SelectInput.tsx
@@ -0,0 +1,20 @@
+export default function SelectInput({onChange, isValid, options}) {
+	return (
+		<div>
+			{/* <label for="country">Velg land:</label> */}
+
+			<select name="country"  id="country" onChange={onChange} defaultValue="">
+				<option value="" disabled>Velg et alternativ</option>
+
+				{options.map((option) => (
+					<option key={option.value}> {option.label} </option>
+				))}
+			</select>
+
+			<div 
+				className={`form__input-message ${isValid ? '' : 'form__input-message--visible'}`}>
+					Feil: Velg et alternativ.
+			</div>
+		</div>
+	)
+}
