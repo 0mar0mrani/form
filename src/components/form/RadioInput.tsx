d5bd60bc26170f1d85969acabe1cf42879899ea4
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2022-12-28 11:52:52 +0100

    Added border around all radio options

--- src/components/form/RadioInput.tsx
+++ src/components/form/RadioInput.tsx
@@ -3,19 +3,21 @@ export default function SelectInput({ name, onChange, isValid, options}) {
 		<div className="form__radio">
 			<div htmlFor="" className="form__radio-header">{ name }</div>
 
-			{options.map((option) => (
-				<div key={option.value}>
-					<label htmlFor={option.value}> {option.label} </label>
+			<div className="form__radio-container">
+				{options.map((option) => (
+					<div key={option.value}>
+						<label htmlFor={option.value}> {option.label} </label>
 
-					<input
-						type="radio" 
-						value={option.value} 
-						id={option.value} 
-						name="gender" 
-						onChange={onChange}
-					/>	
-				</div>
-			))}
+						<input
+							type="radio" 
+							value={option.value} 
+							id={option.value} 
+							name="gender" 
+							onChange={onChange}
+						/>	
+					</div>
+				))}
+			</div>
 
 			<div 
 				className={`form__input-message ${isValid ? '' : 'form__input-message--visible'}`}>
