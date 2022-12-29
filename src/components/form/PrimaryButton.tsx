985ff9ef3b99d7b430ee7df10fbb53a1554326b8
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2022-12-29 21:34:37 +0100

    Disabled button through html

--- src/components/form/PrimaryButton.tsx
+++ src/components/form/PrimaryButton.tsx
@@ -3,6 +3,7 @@ import { React } from 'react'
 export default function Button({name, submitMethod, isDisabled }) {
 	return (
 		<button
+			disabled={isDisabled}
 			className={`form__button-primary ${isDisabled ? 'form__button-primary--disabled' : ''}`}
 			onClick={submitMethod}>
 			{name}
